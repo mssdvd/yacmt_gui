1b7f1e8e517b8c5d6e179e0962fadbbbf395e353
Davide Masserut <d.masserut@gmail.com>
2018-05-29 21:29:38 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='yacmt_gui',
+    version='0.1',
+    author='Davide Masserut',
+    author_email='d.masserut@gmail.com',
+    packages=['yacmt_gui'],
+    install_requires=['pyqt5', 'requests'],
+    extras_require={'dev': ['flake8']},
+    entry_points={'console_scripts': ['yacmt_gui = main:main']})
