8f8411dca359d0f4e9c797b38a745e7255ef643a
Davide Masserut <d.masserut@gmail.com>
2018-06-02 16:37:06 +0200

    Add more widget

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import datetime
 import sys
 
 import requests
 from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import (QApplication, QComboBox, QGridLayout, QHBoxLayout,
-                             QLabel, QLCDNumber, QMainWindow, QPushButton,
+from PyQt5.QtWidgets import (QApplication, QGridLayout, QLabel, QLCDNumber,
                              QWidget)
 
 from db import init_db, insert_report
 
 
-class YACMT(QWidget):
+class YacmtGUI(QWidget):
     def __init__(self):
         super().__init__()
         init_db()
@@ -17,15 +17,36 @@ class YACMT(QWidget):
 
     def initUI(self):
         grid = QGridLayout()
-        self.lcdSpeed = QLCDNumber()
-        grid.addWidget(self.lcdSpeed)
-        self.lcdRPM = QLCDNumber()
-        grid.addWidget(self.lcdRPM)
 
+        # Speed
+        self.lcdSpeed = QLCDNumber()
+        grid.addWidget(self.lcdSpeed, 0, 0)
+        labelSpeed = QLabel(self.lcdSpeed)
+        labelSpeed.setText("Speed")
+
+        # RPM
+        self.lcdRPM = QLCDNumber()
+        grid.addWidget(self.lcdRPM, 0, 1)
+        labelRPM = QLabel(self.lcdRPM)
+        labelRPM.setText("RPM")
+
+        # Control Module voltage
+        self.lcdCMV = QLCDNumber()
+        grid.addWidget(self.lcdCMV, 1, 0)
+        labelCMV = QLabel(self.lcdCMV)
+        labelCMV.setText("CMV")
+
+        # Run time
+        self.lcdRunTime = QLCDNumber()
+        self.lcdRunTime.setDigitCount(8)
+        grid.addWidget(self.lcdRunTime)
+        labelRunTime = QLabel(self.lcdRunTime)
+        labelRunTime.setText("Run Time")
+
+        # Set timers
         self.display_timer = QTimer()
         self.display_timer.timeout.connect(self._update_display)
         self.display_timer.start(1000)
-
         self.report_timer = QTimer()
         self.report_timer.timeout.connect(self._new_report)
         self.report_timer.start(5000)
@@ -36,9 +57,12 @@ class YACMT(QWidget):
         self.show()
 
     def _update_display(self):
-        yacm_json = requests.get("http://localhost:8080").json()
-        self.lcdRPM.display(yacm_json.get("eng_rpm"))
-        self.lcdSpeed.display(yacm_json.get("speed"))
+        yacmt_json = requests.get("http://localhost:8080").json()
+        self.lcdRPM.display(yacmt_json.get("eng_rpm"))
+        self.lcdSpeed.display(yacmt_json.get("speed"))
+        self.lcdCMV.display(yacmt_json.get("control_mod_voltage"))
+        self.lcdRunTime.display(
+            str(datetime.timedelta(seconds=yacmt_json.get("run_time"))))
 
     def _new_report(self):
         yacm_json = requests.get("http://localhost:8080").json()
@@ -47,7 +71,7 @@ class YACMT(QWidget):
 
 def main():
     app = QApplication(sys.argv)
-    ex = YACMT()
+    yacmt_gui = YacmtGUI()
     sys.exit(app.exec_())
 
 
