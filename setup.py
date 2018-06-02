b04104aa25cfd91e7901446c3d4a7546a66d7e66
Davide Masserut <d.masserut@gmail.com>
2018-06-02 16:56:57 +0200

    Add missing dependecies to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author='Davide Masserut',
     author_email='d.masserut@gmail.com',
     packages=['yacmt_gui'],
-    install_requires=['pyqt5', 'requests'],
+    install_requires=['pyqt5', 'requests', 'psycopg2', 'sqlalchemy'],
     extras_require={'dev': ['flake8']},
     entry_points={'console_scripts': ['yacmt_gui = main:main']})
