b8b0038cd3784377e6f3f6cb8d6b4a3b269d09b6
Davide Masserut <d.masserut@gmail.com>
2018-06-03 16:58:15 +0200

    Release 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='yacmt_gui',
-    version='0.2.1',
+    version='0.2.2',
     author='Davide Masserut',
     author_email='d.masserut@gmail.com',
     packages=['yacmt_gui'],
