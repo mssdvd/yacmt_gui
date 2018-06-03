a1914b39cc19ab8380b0c0d2ec9bac7a0aab04a9
Davide Masserut <d.masserut@gmail.com>
2018-06-03 16:57:33 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author='Davide Masserut',
     author_email='d.masserut@gmail.com',
     packages=['yacmt_gui'],
-    install_requires=['pyqt5', 'requests', 'psycopg2', 'sqlalchemy'],
+    install_requires=['yacmt', 'PyQt5', 'requests', 'psycopg2', 'sqlalchemy'],
     extras_require={'dev': ['flake8']},
     entry_points={'console_scripts': ['yacmt_gui = yacmt_gui.main:main']})
