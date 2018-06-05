8363fd2b6cf8a07ac602c04cde701a695817e2bd
Davide Masserut <d.masserut@gmail.com>
2018-06-05 15:09:29 +0200

    Release 0.2.5

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@ from setuptools import setup
 
 setup(
     name='yacmt_gui',
-    version='0.2.3',
+    version='0.2.5',
     author='Davide Masserut',
     author_email='d.masserut@gmail.com',
     packages=['yacmt_gui'],
-    install_requires=[
-        'yacmt', 'PyQt5', 'requests', 'psycopg2', 'psycopg2-binary',
-        'sqlalchemy'
-    ],
+    install_requires=['yacmt', 'PyQt5', 'psycopg2', 'sqlalchemy'],
     extras_require={'dev': ['flake8']},
     entry_points={'console_scripts': ['yacmt_gui = yacmt_gui.main:main']})
