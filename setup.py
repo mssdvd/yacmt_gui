6dc6c5c57609625f693285a796a6877b53f88cfb
Davide Masserut <d.masserut@gmail.com>
2018-06-12 21:15:14 +0200

    Specify license in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.3',
     author='Davide Masserut',
     author_email='d.masserut@gmail.com',
+    license='MIT',
     packages=['yacmt_gui'],
     install_requires=['yacmt', 'PyQt5', 'psycopg2', 'sqlalchemy'],
     extras_require={'dev': ['flake8']},
