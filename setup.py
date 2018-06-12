ad5a6aeffc2097be3202738773e5f2477b1bfaf5
Davide Masserut <d.masserut@gmail.com>
2018-06-13 01:21:39 +0200

    Update dependecy name

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author_email='d.masserut@gmail.com',
     license='MIT',
     packages=['yacmt_gui'],
-    install_requires=['yacmt', 'PyQt5', 'psycopg2', 'sqlalchemy'],
+    install_requires=['yacmt_core', 'PyQt5', 'psycopg2', 'sqlalchemy'],
     extras_require={'dev': ['flake8']},
     entry_points={'console_scripts': ['yacmt_gui = yacmt_gui.main:main']})
